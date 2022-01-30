5072b3c0ef6d6d29be26db6b99cd3d850dd6e325
TosinHub <tosinolugbenga@gmail.com>
2022-01-30 17:09:17 +0100

    s

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import expresss from 'express'
 import bodyParser  from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import postRoutes from './routes/posts.js'
 
 
 const app = expresss();
+
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({limit : "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit : "30mb", extended: true}))
